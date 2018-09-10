b95be93f16b4be6ab960b6ff49aee53ca25e0225
telostia <telostia@gmail.com>
2018-09-10 12:27:19 +1000

    updated

--- guides/masternode_auto.sh
+++ guides/masternode_auto.sh
@@ -23,7 +23,7 @@ rm mymn*
 #get wallet files
 wget https://github.com/MyMNProject/mymn-guides/raw/master/wallet/linux64/mymn-linux.tar.gz
 sudo tar -xvf mymn-linux.tar.gz
-sudo rm mymn-linux* mymn_auto.sh
+sudo rm mymn-linux* masternode_auto.sh
 sudo chmod +x mymn*
 sudo cp mymn* /usr/local/bin
 sudo ufw allow 10261/tcp
@@ -42,7 +42,7 @@ echo -e "${GREEN}Preparing config file ${NONE}";
 rm -rf $HOME/.mymn
 sudo mkdir $HOME/.mymn
 
-printf "addnode=139.99.168.121:10261\naddnode=139.99.159.113:10261\naddnode=139.99.159.77:10261\naddnode=139.99.197.112:10261\naddnode=139.99.197.135:10261\naddnode=139.99.196.73:10261\naddnode=139.99.202.60:10261\naddnode=139.99.158.38:10261\n\nrpcuser=mymn$USER\nrpcpassword=$PASSW\\ndaemon=1\nlisten=1\nrpcport=66541\nrpcallowip=127.0.0.1\nserver=1\nmaxconnections=54\nexternalip=$EXTIP:10261\nmasternode=1\nmasternodeprivkey=$MNKEY" >  $HOME/.mymn/mymn.conf
+printf "addnode=139.99.168.121:10261\naddnode=139.99.159.113:10261\naddnode=139.99.159.77:10261\naddnode=139.99.197.112:10261\naddnode=139.99.197.135:10261\naddnode=139.99.196.73:10261\naddnode=139.99.202.60:10261\naddnode=139.99.158.38:10261\n\nrpcuser=mymn$USER\nrpcpassword=$PASSW\\ndaemon=1\nlisten=1\nrpcport=66541\nrpcallowip=127.0.0.1\nserver=1\nmaxconnections=154\nexternalip=$EXTIP:10261\nmasternode=1\nmasternodeprivkey=$MNKEY" >  $HOME/.mymn/mymn.conf
 
 mymnd
 watch mymn-cli getinfo
