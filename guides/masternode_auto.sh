89d08617a15e6dad41e2e36bcc34c96e525ccb7d
telostia <telostia@gmail.com>
2018-09-10 12:04:35 +1000

    fixed script

--- guides/masternode_auto.sh
+++ guides/masternode_auto.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+sudo apt-get install git -y
+sudo apt-get install nano -y
+sudo apt-get install curl -y
+sudo apt-get install pwgen -y
+sudo apt-get install wget -y
+sudo apt-get install build-essential libtool automake autoconf -y
+sudo apt-get install autotools-dev autoconf pkg-config libssl-dev -y
+sudo apt-get install libgmp3-dev libevent-dev bsdmainutils libboost-all-dev -y
+sudo apt-get install libzmq3-dev -y
+sudo apt-get install libminiupnpc-dev -y
+sudo add-apt-repository ppa:bitcoin/bitcoin -y
+sudo apt-get update -y
+sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
 
 cd
 #remove old files
